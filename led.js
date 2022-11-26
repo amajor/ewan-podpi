35c9a65ed42ea128ae0a420086060a9ea9080a2b
Alison Major <796753+amajor@users.noreply.github.com>
2022-11-25 21:19:19 -0500

    Adventure #2 - using LEDs
    We also used variables to store our values

--- led.js
+++ led.js
@@ -1,7 +1,10 @@
 var five = require("johnny-five");
 var board = new five.Board();
 
+const arduino_pin = 2;
+const blinking_interval = 5006; // Milliseconds
+
 board.on("ready", function() {
-  var led = new five.Led(3);
-  led.blink(500);
+  var led = new five.Led(arduino_pin); // Arduino Pin
+  led.blink(blinking_interval); // Blinking Interval in Milliseconds
 });
